2b7dad515403a8ddb30b3c4bc8bf7b7b8964b50a
Medson Kasese <medsonkasese683@gmail.com>
2026-06-14 16:58:12 +0200

    Create firebaseConfig.js

---
+++ js/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBERoeYLfG93LGYleo7GW0mxeOMYF1F2b8",
+  authDomain: "snapnotes-uid2002.firebaseapp.com",
+  projectId: "snapnotes-uid2002",
+  storageBucket: "snapnotes-uid2002.firebasestorage.app",
+  messagingSenderId: "398214917269",
+  appId: "1:398214917269:web:ba3f293f8b13bdd966ff9c"
+};
+
+export const app = initializeApp(firebaseConfig);
